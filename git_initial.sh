c45d85ca3e672208537c2a5d0dd910e6687c08f5
Paul Chang <superchocolatelover@gmail.com>
2022-12-22 12:25:11 +0800

    My first commit in short.
    Detailed description here.

---
+++ git_initial.sh
@@ -0,0 +1,19 @@
+git --version
+# show the git version
+
+git config --global user.name "Paul Chang"
+# set up the username
+
+git config --global user.email superchocolatelover@gmail.com
+# set up the email
+
+git config --global core.editor "code --wait"
+# set the text editor to vs code
+
+git config --global -e  
+# edit the config file
+
+git config --global core.autocrlf input
+# for end of line mark
+# for mac, set to "input"
+# for windows, set to "true"
