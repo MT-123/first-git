d44f38c629924da77f872f8bcdd100ba1a0987fa
Paul Chang <superchocolatelover@gmail.com>
2022-12-28 12:46:04 +0800

    add github note 2

---
+++ github_note2.sh
@@ -0,0 +1,7 @@
+git fetch origin branch_name
+# dowload the files from remote repo origin
+# if no branch name, master brnach in the remote repo will be downloaded
+# a new branch will be auto created
+
+git pull origin branch_name
+# fetch and merge to the current branch
