a3e5c6b115e1815a2e7f0aa4bd7852062b885b5b
Paul Chang <superchocolatelover@gmail.com>
2022-12-28 12:54:51 +0800

    update github note 2

--- github_note2.sh
+++ github_note2.sh
@@ -1,7 +1,17 @@
 git fetch origin branch_name
 # dowload the files from remote repo origin
 # if no branch name, master brnach in the remote repo will be downloaded
-# a new branch will be auto created
+# a new branch will be created at remote branch(type of branch, but not the branch at the remote repo)
+
+git branch -r
+# check the remote branch
+
+git merge remote_branch_name
+# merge the remote branch to the current branch
+
+git branch -rd branch_name
+# delete the remote branch(not the branch at the remote repo)
 
 git pull origin branch_name
 # fetch and merge to the current branch
+# pull = fetch + merge
