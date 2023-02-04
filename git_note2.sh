83d50182c935b47560ec895b1328522303fd1d9c
Paul Chang <superchocolatelover@gmail.com>
2023-02-04 15:13:14 +0800

    revise git reset note

--- git_note2.sh
+++ git_note2.sh
@@ -7,6 +7,14 @@ git reset HEAD~2
 git reset ac5f9ba
 # go back to the commit with id
 
+git reset --mixed HEAD^
+git reset --soft HEAD^
+git reset --hard HEAD^
+# --mixed changed only on stage
+# --soft made no change
+# --hard changed on both stage and working directory
+
+
 git diff branch..branch2
 # compare the difference of the 2 branches
 git diff branch1 branch2
